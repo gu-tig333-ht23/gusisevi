808708379b6d417e5c570035574a802939e886c5
bohmaniac <vincent.isegranbohman@gmail.com>
2023-09-07 09:42:30 +0200

    Week 1 completed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,206 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return CupertinoApp(
+      title: 'Vincent To Do',
+      theme: CupertinoThemeData(
+        primaryColor: CupertinoColors.systemRed,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class TaskItem extends StatelessWidget {
+  final String task;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  TaskItem({
+    super.key,
+    required this.task,
+    // TO DO: isCompleted state
+  });
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(task, style: TextStyle(fontSize: 20)),
+          ),
+          CupertinoButton(
+            onPressed: () {
+              print("Completed button pressed");
+              // TO DO: Implement completed state
+              HapticFeedback.selectionClick();
+            },
+            child:
+                Icon(CupertinoIcons.circle, color: CupertinoColors.systemGrey),
+          ),
+          SizedBox(width: 10),
+          CupertinoButton(
+            onPressed: () {
+              print("Delete button pressed");
+              // TO DO: Implement delete
+            },
+            child: Icon(CupertinoIcons.delete,
+                color: CupertinoColors.destructiveRed),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<String> tasks = [
+    'Write a book',
+    'Do homework',
+    'Tidy room',
+    'Watch TV',
+    'Nap',
+    'Shop groceries',
+    'Have fun',
+    'Meditate',
+    // TO DO: Implement proper, flexible data structure for this
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Stack(
+        children: [
+          CustomScrollView(
+            slivers: <Widget>[
+              CupertinoSliverNavigationBar(
+                largeTitle: Text('Vincent To Do'),
+                trailing: CupertinoButton(
+                  padding: EdgeInsets.zero,
+                  color: CupertinoTheme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(25.0),
+                  child: Icon(CupertinoIcons.ellipsis),
+                  onPressed: () {
+                    print("Menu Button Pressed");
+                    _showMenu(context);
+                  },
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    return TaskItem(
+                      task: tasks[index],
+                    );
+                  },
+                  childCount: tasks.length,
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            left: 16,
+            right: 16,
+            bottom: 60,
+            child: CupertinoButton.filled(
+              child: Text('Add Task'),
+              onPressed: () {
+                print("Add Task Button Pressed");
+                HapticFeedback.selectionClick();
+                Navigator.of(context).push(CupertinoPageRoute(
+                  builder: (context) => AddTaskPage(),
+                ));
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void _showMenu(BuildContext context) {
+  showCupertinoModalPopup(
+    context: context,
+    builder: (context) => CupertinoActionSheet(
+      title: Text('Show'),
+      actions: [
+        CupertinoActionSheetAction(
+          child: Text('All'),
+          onPressed: () {
+            // TO DO: Handle "All" action here
+            Navigator.pop(context);
+          },
+        ),
+        CupertinoActionSheetAction(
+          child: Text('Done'),
+          onPressed: () {
+            // TO DO: Handle "Done" action here
+            Navigator.pop(context);
+          },
+        ),
+        CupertinoActionSheetAction(
+          child: Text('Undone'),
+          onPressed: () {
+            // TO DO: Handle "Undone" action here
+            Navigator.pop(context);
+          },
+        ),
+      ],
+      cancelButton: CupertinoActionSheetAction(
+        isDefaultAction: true,
+        child: Text('Cancel'),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    ),
+  );
+}
+
+class AddTaskPage extends StatelessWidget {
+  final TextEditingController taskController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Add Task'),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CupertinoTextField(
+              controller: taskController,
+              placeholder: 'What are you going to do?',
+            ),
+            SizedBox(height: 20),
+            CupertinoButton.filled(
+              child: Text('Add'),
+              onPressed: () {
+                String taskName = taskController.text.trim();
+                if (taskName.isNotEmpty) {
+                  // TODO: Add task to tasks list
+                  print('New Task: $taskName');
+                } else {
+                  // TODO: Handle empty task
+                  print('Empty task');
+                }
+                Navigator.of(context).pop();
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
